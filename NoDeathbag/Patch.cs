06836c05164fced680c78e7835522ca340cee14d
kruftt <nnrbgb@gmail.com>
2025-05-13 01:50:39 -0700

    fix NoDeathbag Description

--- NoDeathbag/Patch.cs
+++ NoDeathbag/Patch.cs
@@ -8,17 +8,9 @@ class NoDeathbagPatch
     [HarmonyPatch(typeof(LocalPlayer), nameof(LocalPlayer.OnEntityDeath))]
     private static bool OnEntityDeath_SkipDeathbag(LocalPlayer __instance)
     {
-        __instance._cameraManager.GetModule<CameraVisualSettings>().AddVisualAction(new BloomFlash(1f, 4f));
-        __instance._cameraManager.GetModule<CameraVisualSettings>().AddVisualAction(__instance._deathCameraEffect);
+        var _settings = __instance._cameraManager.GetModule<CameraVisualSettings>();
+        _settings.AddVisualAction(new BloomFlash(1f, 4f));
+        _settings.AddVisualAction(__instance._deathCameraEffect);
         return false;
     }
-
-    //static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-    //{
-    //    return new CodeMatcher(instructions)
-    //        .MatchForward(false, new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(WildSkies.Player.LocalPlayer), "_playerInventoryService")))
-    //        .MatchBack(false, new CodeMatch(OpCodes.Ldarg_0))
-    //        .Insert(new CodeInstruction(OpCodes.Ret))
-    //        .Instructions();
-    //}
 }
